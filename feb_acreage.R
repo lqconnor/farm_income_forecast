1e0a0b28be209bf16efd56bc17d6e8391b957096
Lawson Connor <connorql@gmail.com>
2018-03-29 13:17:07 -0400

    Pre-amble

--- feb_acreage.R
+++ feb_acreage.R
@@ -1,2 +1,12 @@
 # Farm Income Forecast Project.
 # Testing the effect of February Farm Income Forecasts on Planted Acreage
+
+rm(list = ls())
+cat("\f")
+getwd()
+
+library(tidyverse)
+library(stringr)
+library(magrittr)
+library(lubridate)
+library(ggplot2)
