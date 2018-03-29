35a0e313af875a5ca1f981598b92c863915cd8c6
Lawson Connor <connorql@gmail.com>
2018-03-29 17:52:42 -0400

    Cleaned up graphs. Added relationship between february+1 and final estimate

--- feb_acreage.R
+++ feb_acreage.R
@@ -1,5 +1,5 @@
 # Farm Income Forecast Project.
-# Testing the effect of February Farm Income Forecasts on Planted Acreage
+# The existence of smoothing in farm income forecasts
 
 #Pre - Amble ------------------------------
 rm(list = ls())
@@ -19,12 +19,23 @@ inc_fcst <- read_csv("./Data/forecasts.csv")
 # Variable Gen --------------------------------
 inc_fcst <- mutate(inc_fcst, aug_rev = (`August forecast` - `February forecast`)/`February forecast`,
                    nov_rev = (`November forecast` - `August forecast`)/`August forecast`,
-                   feb_rev = (`February(t+1) forecast` - `November forecast`)/`November forecast`)
+                   feb_rev = (`February(t+1) forecast` - `November forecast`)/`November forecast`,
+                   aug_est = (`August (t + 1) "estimate"` - `February(t+1) forecast`)/`February(t+1) forecast`,
+                   final_est = (`Net farm income estimate` - `February(t+1) forecast`)/`February(t+1) forecast`)
+
 
 ggplot(data = inc_fcst, aes(x=aug_rev, y=nov_rev)) +
   geom_point() +
   geom_smooth(method = lm, se = FALSE)
 
-ggplot(data = inc_fcst) +
-  geom_point(aes(x=nov_rev, y=feb_rev)) +
-  geom_smooth(aes(x=nov_rev, y=feb_rev), method = lm)
+ggplot(data = inc_fcst, aes(x=nov_rev, y=feb_rev)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE)
+
+ggplot(data = inc_fcst, aes(x=feb_rev, y=aug_est)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE)
+
+ggplot(data = inc_fcst, aes(x=feb_rev, y=final_est)) +
+  geom_point() +
+  geom_smooth(method = lm, se = FALSE)
